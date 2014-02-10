12537d6e181d2715ce8298f4194fd6b417c572c3
general3214 <phodragon@hotmail.com>
2014-02-10 13:25:46 -0800

    Grammar nazi, I am

--- tk/rongmario/extradisks/ExtraDisks.java
+++ tk/rongmario/extradisks/ExtraDisks.java
@@ -19,8 +19,8 @@ import java.util.Arrays;
 @NetworkMod(clientSideRequired = true, serverSideRequired = false)
 public class ExtraDisks
 {
-    public static final String MODID = "extradisks";
-    public static final String NAME = "Extra Music Disks";
+    public static final String MODID = "extradiscs";
+    public static final String NAME = "Extra Music Discs";
     public static final String VERSION = "1.0.0";
     public static final String ASSETS = MODID + ":";
 
