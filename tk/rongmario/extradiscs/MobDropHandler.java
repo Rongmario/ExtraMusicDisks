431907e0d019a5dbc02cbb6e4d4f147b217bac5d
general3214 <phodragon@hotmail.com>
2014-03-01 10:50:21 -0800

    Fixed my fail

--- tk/rongmario/extradiscs/MobDropHandler.java
+++ tk/rongmario/extradiscs/MobDropHandler.java
@@ -17,33 +17,33 @@ public class MobDropHandler {
 		if (event.source.getDamageType().equals("player")) { //Checks for Damage Type.
 			rand = Math.random(); //Initializes double "rand"
 			if (event.entityLiving instanceof EntityZombie || event.entityLiving instanceof EntitySkeleton || event.entityLiving instanceof EntityCreeper) { 
-				if (rand < 0.5000D) // 50% drop chance. Example: (0.25D = 25%, 1D = 100%, etc.);
+				if (rand <= 0.5D) // 50% drop chance. Example: (0.25D = 25%, 1D = 100%, etc.);
 					event.entityLiving.dropItem(ExtraDiscs.recordDubstep1.itemID, 1);
-				if (rand < 0.4999D) // idk why, but repeating "if (rand < 0.5D)" won't work
+				else if (rand <= 0.5001D) // idk why, but repeating "if (rand < 0.5D)" won't work
 					event.entityLiving.dropItem(ExtraDiscs.recordDubstep2.itemID, 1);
-				if (rand < 0.4998D)
+				else if (rand <= 0.5002D)
 					event.entityLiving.dropItem(ExtraDiscs.recordClassical1.itemID, 1);
-				if (rand < 0.4997D)
+				else if (rand <= 0.5003D)
 					event.entityLiving.dropItem(ExtraDiscs.recordNightOwl.itemID, 1);
-				if (rand < 0.4996D)
+				else if (rand <= 0.5004D)
 					event.entityLiving.dropItem(ExtraDiscs.recordEclosion.itemID, 1);
-				if (rand < 0.4995D)
+				else if (rand <= 0.5005D)
 					event.entityLiving.dropItem(ExtraDiscs.recordChiptune1.itemID, 1);
-				if (rand < 0.4994D)
+				else if (rand <= 0.5006D)
 					event.entityLiving.dropItem(ExtraDiscs.recordChiptune2.itemID, 1);
-				if (rand < 0.4993D)
+				else if (rand <= 0.5007D)
 					event.entityLiving.dropItem(ExtraDiscs.recordChiptune3.itemID, 1);
-				if (rand < 0.4992D)	
+				else if (rand <= 0.5008D)	
 					event.entityLiving.dropItem(ExtraDiscs.recordZabriskie1.itemID, 1);
-				if (rand < 0.4991D)
+				else if (rand <= 0.5009D)
 					event.entityLiving.dropItem(ExtraDiscs.recordZabriskie2.itemID, 1);
-				if (rand < 0.4990D)
+				else if (rand <= 0.5010D)
 					event.entityLiving.dropItem(ExtraDiscs.recordRVegners.itemID, 1);
-				if (rand < 0.4989D)
+				else if (rand <= 0.5010D)
 					event.entityLiving.dropItem(ExtraDiscs.recordGoobyPls.itemID, 1);
-				if (rand < 0.4988D)
+				else if (rand <= 0.5011D)
 					event.entityLiving.dropItem(ExtraDiscs.recordEerie1.itemID, 1);
-				if (rand < 0.4987D)
+				else if (rand <= 0.5012D)
 					event.entityLiving.dropItem(ExtraDiscs.recordPony1.itemID, 1);
 			  }
 	        }
